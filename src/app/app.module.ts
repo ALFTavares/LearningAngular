ff6ca292c069547440e7e6320ef96e9095b6c62f
mouse <alftavares1985@gmail.com>
2018-06-22 09:03:20 +0100

    refactored app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -13,30 +13,36 @@ import { WelcomeComponent } from './home/welcome.component';
 import { ProductDetailGuard } from './products/product-guard.service';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot([
-        { path: 'products', component: ProductListComponent },
-        { path: 'product/:id',
-            canActivate: [ ProductDetailGuard ],
-            component: ProductDetailComponent },
-        { path: 'welcome', component: WelcomeComponent },
-        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-        { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductFilterPipe,
-    StarComponent,
-    ProductDetailComponent,
-    ProductListComponent,
-    WelcomeComponent
-  ],
-  providers: [ ProductDetailGuard ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot([
+            { path: 'products', component: ProductListComponent },
+            {
+                path: 'product/:id',
+                canActivate: [ProductDetailGuard],
+                component: ProductDetailComponent
+            },
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+        ])
+    ],
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        ProductFilterPipe,
+        StarComponent,
+        ProductDetailComponent,
+        ProductListComponent,
+        WelcomeComponent
+    ],
+    providers: [
+        ProductDetailGuard
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
