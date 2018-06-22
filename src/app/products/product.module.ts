cc77c55d404a9b0e5af2bc424230940ea1cff0ec
mouse <alftavares1985@gmail.com>
2018-06-22 11:57:56 +0100

    refactored into several modules

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -2,23 +2,18 @@ import { NgModule } from "@angular/core";
 import { ProductListComponent } from "./product-list.component";
 import { ProductDetailComponent } from "./product-detail.component";
 import { ProductFilterPipe } from "./product-filter.pipe";
-import { StarComponent } from "../shared/star.component";
-import { FormsModule } from "@angular/forms";
-import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { ProductDetailGuard } from "./product-guard.service";
 import { ProductService } from "./product.service";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
         ProductFilterPipe,
-        StarComponent
     ],
     imports: [
-        FormsModule,
-        CommonModule,
         RouterModule.forChild([
             { path: 'products', component: ProductListComponent },
             {
@@ -26,7 +21,8 @@ import { ProductService } from "./product.service";
                 canActivate: [ProductDetailGuard],
                 component: ProductDetailComponent
             }
-        ])
+        ]),
+        SharedModule
     ],
     providers: [
         ProductService,
