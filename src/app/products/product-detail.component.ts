8b632dc3224d567d5cfb942c276b3b54bae9e549
mouse <alftavares1985@gmail.com>
2018-06-22 08:56:44 +0100

    added guards for safety routing

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { IProduct } from "./product";
 import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
     templateUrl: './product-detail.component.html'
@@ -7,4 +8,16 @@ import { Component } from "@angular/core";
 export class ProductDetailComponent {
     pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute,
+                private _router: Router) {}
+
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += ` : ${id}`;
+    }
+
+    onBack(): void {
+        this._router.navigate(['/products']);
+    }
 }
